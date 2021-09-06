d53724ee064802cd5dda4c72058a10fb37f38875
Ing. Fabio <fcastellanos782@gmail.com>
2021-09-06 16:35:19 -0500

    ajuste responsivo en la vista principal

--- src/App.js
+++ src/App.js
@@ -17,20 +17,24 @@ function App() {
   const prev = () => setSkip(skip - 3);
 
   return (
-    <div className="px-5 bg-gradient">
+    <div className="px-5 sm:px-14 lg:px-60 bg-gradient">
       <div className="flex justify-center py-5 text-3xl text-white border-b-2 border-gradient">
         <IconoApp />
       </div>
-      <h1 className="mb-5 text-3xl font-extrabold text-center mt-7 color-title">
-        Donations
-      </h1>
-      <select className="w-full p-2 mb-5 font-bold text-white bg-black bg-opacity-0 border border-white rounded-xl">
-        <option value="">Sort By Donations</option>
-      </select>
+      <div className="sm:grid-cols-2 sm:grid sm:pt-12 sm:mb-2">
+        <h1 className="mb-5 text-3xl font-extrabold text-center sm:mt-0 mt-7 color-title">
+          Donations
+        </h1>
+        <div className="flex items-center justify-end">
+          <select className="w-full p-2 mb-5 font-bold text-white bg-black bg-opacity-0 border border-white sm:w-3/4 sm:w-3/3 rounded-xl">
+            <option value="">Sort By Donations</option>
+          </select>
+        </div>
+      </div>
       {datosApi.map((donation) => (
         <Card donation={donation} key={donation.first_name} />
       ))}
-      <div className="grid grid-cols-2 gap-4 px-16 pb-10">
+      <div className="grid grid-cols-2 gap-4 px-16 pb-10 sm:px-72">
         {skip > 0 && (
           <button onClick={prev} className="py-2 border-2 border-button">
             Prev
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import { IconArrowRight } from "../helpers/IconArrowRight";
 
 const Card = ({ donation }) => {
   return (
-    <div className="grid grid-cols-4 gap-2 p-4 mb-3 border-2 rounded-xl border-gradient sm:grid-cols-8">
+    <div className="grid grid-cols-4 gap-2 p-4 mb-3 border-2 rounded-xl border-gradient sm:grid-cols-8 sm:py-6">
       <div>
         <img
           className="bg-purple-500 rounded-full "
@@ -11,12 +11,12 @@ const Card = ({ donation }) => {
           width="500px"
         />
       </div>
-      <div className="col-span-3 text-3xl font-bold text-white">
+      <div className="col-span-3 text-3xl font-bold text-white sm:border-r-2">
         <h2>{donation.first_name}</h2>
         <h2>{donation.last_name}</h2>
       </div>
-      <div className="col-span-4">
-        <hr></hr>
+      <div className="col-span-4 sm:sr-only">
+        <hr />
       </div>
       <div className="col-span-2 font-bold text-white">
         <h2>Total Donations</h2>
